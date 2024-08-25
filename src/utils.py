e0717abcffbcefc8f02d742f9e0e53a8f4eb8b0c
SaINallamothu <sairamnallamothu@gmail.com>
2024-08-25 23:00:22 +0200

    preprocessing.pkl is added

--- src/utils.py
+++ src/utils.py
@@ -32,7 +32,7 @@ def load_csv(folderpath: str):
 def save_obj(file_path, obj):
     try:
         dir_path= os.path.dirname(file_path)
-        os.makedirs(dir_path)
+        os.makedirs(dir_path, exist_ok=True)
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
     except Exception as e:
